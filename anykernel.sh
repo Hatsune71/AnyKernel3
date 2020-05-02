4aa5e5323152d93f5f121570e509143c3bfe82c4
Hatsune-Miku <49642240+Hatsune71@users.noreply.github.com>
2020-05-02 13:52:28 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Custom Miku Kernel by Hatsune71 Based on Qwerty and ProjektEva Kernel.
+kernel.string=Custom Miku Kernel by Hatsune71 Based on ProjektEva Kernel.
 do.devicecheck=0
 do.modules=0
 do.systemless=0
